d53378eed2135504dcc71e782a0eb5ee9278732c
21120461 - Dam Hong Hung <damhonghung123@gmail.com>
2024-07-18 22:49:41 +0700

    Update pom.xml

--- backend/comic/src/test/java/com/group17/comic/plugins/exporter/concretes/DocxConverterTest.java
+++ backend/comic/src/test/java/com/group17/comic/plugins/exporter/concretes/DocxConverterTest.java
@@ -2,12 +2,12 @@ package com.group17.comic.plugins.exporter.concretes;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 
 @SpringBootTest
 class DocxConverterTest {
-    @Mock
+    @SpyBean
     private DocxExporter docxConverter;
 
     @Test
